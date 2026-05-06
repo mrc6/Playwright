ee533f16004b12462ac742adf201127fe1aaeb15
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2026-05-06 09:44:10 -0300

    Add files via upload

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -16,3 +16,15 @@ test('should login successfully', async ( {page} ) =>{
     const productTitle = await page.locator('.header_secondary_container > span');
     await expect(productTitle ).toHaveText('Products');
   });
+
+  test('should display error message for invalid credentials', async ( {page} ) =>{
+    await page.goto('https://saucedemo.com');
+    await expect(page).toHaveTitle('Swag Labs');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('random_12345');
+    await page.locator('[data-test="login-button"]').click();
+    
+    //assertion time  
+    const errorText = await page.getByText(/Username and password do not match/);
+    await expect(errorText ).toBeVisible();
+  });
